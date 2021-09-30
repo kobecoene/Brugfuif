80dd21c821377b1defdc2ea90a6395869974a4b7
BuildTools <unconfigured@null.spigotmc.org>
2021-09-30 22:23:39 +0200

    WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+let windowWidth = $(window).width();
+
+$('.main').mousemove(function (event) {
+    let moveX = (($(window).width() / 50) - event.pageX) * 0.01;
+    let moveY = (($(window).height() / 50) - event.pageY) * 0.01;
+
+    $('.wrapper').css('margin-left', moveX + 'px');
+    $('.wrapper').css('margin-top', moveY + 'px');
+});
+
 let constrain = 200;
 let mouseOverContainer = document.getElementById("body");
 let logoLayer = document.getElementById("logo");
@@ -24,13 +34,3 @@ mouseOverContainer.onmousemove = function (e) {
         transformElement(logoLayer, position);
     });
 };
-
-let windowWidth = $(window).width();
-
-$('.main').mousemove(function (event) {
-    let moveX = (($(window).width() / 50) - event.pageX) * 0.01;
-    let moveY = (($(window).height() / 50) - event.pageY) * 0.01;
-
-    $('.wrapper').css('margin-left', moveX + 'px');
-    $('.wrapper').css('margin-top', moveY + 'px');
-});
