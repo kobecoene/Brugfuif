e64388cf31dbc445551dde14cffc8a236143031a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-01 14:02:01 +0200

    Added better responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
 let windowWidth = $(window).width();
 
-$('.main').mousemove(function (event) {
-    let moveX = (($(window).width() / 50) - event.pageX) * 0.01;
-    let moveY = (($(window).height() / 50) - event.pageY) * 0.01;
+if (windowWidth > 1000) {
+    $('.main').mousemove(function (event) {
+        let moveX = (($(window).width() / 50) - event.pageX) * 0.01;
+        let moveY = (($(window).height() / 50) - event.pageY) * 0.01;
 
-    $('.wrapper').css('margin-left', moveX + 'px');
-    $('.wrapper').css('margin-top', moveY + 'px');
-});
+        $('.wrapper').css('margin-left', moveX + 'px');
+        $('.wrapper').css('margin-top', moveY + 'px');
+    });
+}
 
 let constrain = 200;
 let mouseOverContainer = document.getElementById("body");
